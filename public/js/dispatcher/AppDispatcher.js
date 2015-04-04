1e94e135eb3a204acdd74518170a4316f37e0ae8
masanao.aoki <negativetype@gmail.com>
2015-04-04 15:21:05 +0900

    flux化作業のため整理

---
+++ public/js/dispatcher/AppDispatcher.js
@@ -0,0 +1,9 @@
+define(['exports', 'flux'], function (exports, _flux) {
+  'use strict';
+
+  Object.defineProperty(exports, '__esModule', {
+    value: true
+  });
+  var AppDispatcher = new _flux.Dispatcher();
+  exports.AppDispatcher = AppDispatcher;
+});
--- public/js/test.js
+++
@@ -1,8 +0,0 @@
-define(["exports"], function (exports) {
-  "use strict";
-
-  var setting = exports.setting = "react";
-  Object.defineProperty(exports, "__esModule", {
-    value: true
-  });
-});
